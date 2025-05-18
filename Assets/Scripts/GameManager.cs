6cc4c3d36e4ac0a7007fb1c04b7d02dd11271db6
Alexander <alexandr941989@gmail.com>
2025-05-18 16:47:02 +0300

    Spawn crosses on the grid click.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,13 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
+    public event EventHandler<OnClickedOnGridPositionEventArgs> OnClickedOnGridPosition;
+    public class OnClickedOnGridPositionEventArgs : EventArgs
+    {
+        public int x;
+        public int y;
+    }
+    
     private void Awake()
     {
         if (!Instance)
@@ -20,5 +27,10 @@ public class GameManager : MonoBehaviour
     public void ClickedOnGridPosition(int x, int y)
     {
         Debug.LogFormat("Clicked on grid position {0}, {1}", x, y);
+        OnClickedOnGridPosition?.Invoke(this, new OnClickedOnGridPositionEventArgs
+        {
+            x = x,
+            y = y
+        });
     }
 }
