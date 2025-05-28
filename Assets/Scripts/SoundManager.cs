e13bcf4d7ee5fa7ce5922c46f0a2d3a2c6b95682
Alexander Mikhalchenko <alexandr941989@gmail.com>
2025-05-28 17:01:34 +0300

    Add win and lose SFXs.

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -3,16 +3,41 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    private const float AudioCleanupDelay = 5f;
+
     [SerializeField] private Transform placeSfxPrefab;
+    [SerializeField] private Transform winSfxPrefab;
+    [SerializeField] private Transform loseSfxPrefab;
 
     private void Start()
     {
         GameManager.Instance.OnObjectPlaced += GameManager_OnObjectPlaced;
+        GameManager.Instance.OnGameWin += GameManager_OnGameWin;
+    }
+
+    private void OnDisable()
+    {
+            GameManager.Instance.OnObjectPlaced -= GameManager_OnObjectPlaced;
+            GameManager.Instance.OnGameWin -= GameManager_OnGameWin;
+    }
+
+    private void GameManager_OnGameWin(object sender, GameManager.OnGameWinEventArgs e)
+    {
+        var sfxPrefab = e.WinPlayerType == GameManager.Instance.GetLocalPlayerType() 
+            ? winSfxPrefab 
+            : loseSfxPrefab;
+            
+        PlaySound(sfxPrefab);
     }
 
     private void GameManager_OnObjectPlaced(object sender, EventArgs e)
     {
-        var placeSfx = Instantiate(placeSfxPrefab);
-        Destroy(placeSfx.gameObject, 5f);
+        PlaySound(placeSfxPrefab);
+    }
+
+    private void PlaySound(Transform sfxPrefab)
+    {
+        var sfxInstance = Instantiate(sfxPrefab);
+        Destroy(sfxInstance.gameObject, AudioCleanupDelay);
     }
 }
